d05ab3071dbc5aac1535e2fbf7763106d2421349
k <brood915@gmail.com>
2017-04-20 14:29:50 -0400

    added containers and components

--- src/components/input.js
+++ src/components/input.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Input(props) {
-    return (
-    	<div>
-    		Hello World!
-    	</div>
-    )
+class InputContainer extends React.Component {
+  update(event) {
+    const newValue = event.target.value;
+    this.props.updateValue(newValue);
+  }
+  render() {
+    return (<textarea rows= "22"  className="inputContainer" value = {this.props.value} onChange = {this.update.bind(this)}></textarea>);
+  }
 }
 
 Input.propTypes = {
-	
+	value: Proptypes.string.isRequired;
+	updateValue: Proptypes.func.isrequired;
 }
 
 export default Input;
